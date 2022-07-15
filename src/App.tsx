480c3b0e3bb1a4b4eda787f9e9a68e30e8fa627f
Alvaro Martin <48942635+AlvaroMartinNarganes@users.noreply.github.com>
2022-07-15 18:45:43 +0200

    commit desde windows

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import './App.scss';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import {DarkModeSwitch} from 'react-toggle-dark-mode';
+import "./App.scss";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 function App() {
   const [isDarkMode, setDarkMode] = React.useState(true);
-  const [theme,setTheme]=React.useState("dark");
-  const toggleDarkMode = (checked:boolean) => {
-    setTheme(theme==="dark"?"light":"dark");
+  const [theme, setTheme] = React.useState("dark");
+  const toggleDarkMode = (checked: boolean) => {
+    setTheme(theme === "dark" ? "light" : "dark");
     setDarkMode(checked);
-  }
+  };
   return (
     <div className="app">
-       <div className={theme}>
-      <header className="App-header">
-        <p>Modo oscuro: {isDarkMode.toString()}</p>
-      <DarkModeSwitch
-      style={{ marginBottom: '2rem' }}
-      checked={isDarkMode}
-      onChange={toggleDarkMode}
-      size={120}
-    />
-      </header>
+      <div className={theme}>
+        <header className="App-header">
+          <p>Modo oscuro: {isDarkMode.toString()}</p>
+          <DarkModeSwitch
+            style={{ marginBottom: "2rem" }}
+            checked={isDarkMode}
+            onChange={toggleDarkMode}
+            size={50}
+          />
+        </header>
       </div>
     </div>
   );
